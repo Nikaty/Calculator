c9daa20ee80db7e4757044819f89268b66200e5a
Nikaty <nmko.31@gmail.com>
2022-08-09 20:07:18 +0700

    Corrected

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,16 @@ public class Main {
     public static void main(String args[]) throws Exception {
         Scanner scanner = new Scanner(System.in);
         String value = scanner.nextLine();
-        System.out.println(value);
-        if (value.length() > 6)
-        {
-            throw new Exception();
-        }
         System.out.println(calc(value));
     }
 
-    public static String calc(String value)
-    {
+    public static String calc(String value) throws Exception {
         String result = "";
         String[] values = value.split(" ");
+        if (values.length > 3)
+        {
+            throw new Exception();
+        }
         try
         {
             int a = Integer.parseInt(values[0]);
@@ -33,6 +31,10 @@ public class Main {
             String operation = values[1];
             Calculate calculate = new Calculate();
             int res = calculate.getResult(a,b,operation);
+            if (res < 1)
+            {
+                throw new Exception();
+            }
             result = convert.arabicToRomain(res);
         }
 
